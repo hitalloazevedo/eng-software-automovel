96ae7f66880b839c54b92ecf3e79e861e7dca3a0
hitalloazevedo <azevedohitallo@gmail.com>
2025-04-28 07:27:52 -0300

    setup java

---
+++ alocacao_veiculos/src/main/java/com/mycompany/alocacao_veiculos/Alocacao_veiculos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.alocacao_veiculos;
+
+/**
+ *
+ * @author hitallo
+ */
+public class Alocacao_veiculos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
