7e481c96f00d861400121266e921bb9edb67c5cc
hitalloazevedo <azevedohitallo@gmail.com>
2025-04-28 07:48:48 -0300

    criando controllers

---
+++ alocacao_veiculos/src/main/java/com/mycompany/alocacao_veiculos/controller/ClienteController.java
@@ -0,0 +1,14 @@
+package com.mycompany.alocacao_veiculos.controller;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hitallo
+ */
+public class ClienteController {
+    
+}
--- alocacao_veiculos/src/main/java/com/mycompany/alocacao_veiculos/controller/LocacaoController.java
+++ alocacao_veiculos/src/main/java/com/mycompany/alocacao_veiculos/controller/LocacaoController.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.alocacao_veiculos.model;
+package com.mycompany.alocacao_veiculos.controller;
 
 /**
  *
  * @author hitallo
  */
-public class NewClass {
+public class LocacaoController {
     
 }
