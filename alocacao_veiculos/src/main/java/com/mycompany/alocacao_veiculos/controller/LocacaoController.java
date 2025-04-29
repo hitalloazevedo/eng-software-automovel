f72b720cd0288cf306af2768d00f9988764ae625
otbox <otboxmarques88@gmail.com>
2025-04-28 21:29:15 -0300

    git commit locacaoController

--- alocacao_veiculos/src/main/java/com/mycompany/alocacao_veiculos/controller/LocacaoController.java
+++ alocacao_veiculos/src/main/java/com/mycompany/alocacao_veiculos/controller/LocacaoController.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.alocacao_veiculos.controller;
 
-/**
- *
- * @author hitallo
- */
+import java.util.List;
+import java.util.Optional;
+
+import com.mycompany.alocacao_veiculos.model.Locacao;
+import com.mycompany.alocacao_veiculos.model.dao.LocacaoDAO;
+
 public class LocacaoController {
-    
+
+    private LocacaoDAO locacaoDao;
+
+    public LocacaoController(LocacaoDAO locacaoDao) {
+        this.locacaoDao = locacaoDao;
+    }
+
+    public void createLocacao(
+        java.sql.Date dataLocacao,
+        java.sql.Time horaLocacao,
+        long quilometragemLocacao,
+        double valorCaucao
+    ) {
+        try {
+            Locacao newLocacao = new Locacao(dataLocacao, horaLocacao, quilometragemLocacao, valorCaucao);
+            this.locacaoDao.save(newLocacao);
+        } catch (Error err) {
+            System.out.println("Erro ao criar locação: " + err.getMessage());
+        }
+    }
+
+    public Locacao updateLocacao(Long id, double novoValorLocacao, boolean locacaoDevolvida) {
+        Optional<Locacao> oldLocacao = this.locacaoDao.get(id);
+        if (oldLocacao.isPresent()) {
+            Locacao locacao = oldLocacao.get();
+            locacao.setvalorLocacao(novoValorLocacao);
+            locacao.setlocacaoDevolvida(locacaoDevolvida);
+            return locacao;
+        }
+        throw new Error("Locação não encontrada");
+    }
+
+    public void deleteLocacao(Locacao locacao) {
+        this.locacaoDao.delete(locacao);
+    }
+
+    public List<Locacao> findAllLocacoes() {
+        return this.locacaoDao.getAll();
+    }
+
+    public Optional<Locacao> findLocacaoById(Long id) {
+        return this.locacaoDao.get(id);
+    }
 }
